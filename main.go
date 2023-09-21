04c216957cd38b7f5753d8a87a68811b049f7bf8
ROBERTA GRASSO <robertagrasso95@hotmail.com>
2023-09-21 16:51:55 +0200

    for loop part 2

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
 
 
 func main() {
@@ -53,9 +57,13 @@ func main() {
 		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 	
-		fmt.Printf("These are all the bookings: %v\n", bookings)
+		firstNames := []string{}
+		for _, booking := range bookings {
+			var names = strings.Fields(booking) 
+			firstNames = append(firstNames, names[0])
+		}
 
+		fmt.Printf("The first names of bookings are: %v\n", firstNames)
 	}
 	
-
 }
